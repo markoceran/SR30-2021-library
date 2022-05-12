cad40bfd01d9edc9eee12c5b25216900b6a6183d
Marko <101666307+markoceran@users.noreply.github.com>
2022-05-12 23:20:15 +0200

    Osobe

---
+++ Administrator.java
@@ -0,0 +1,20 @@
+package osobe;
+
+public class Administrator extends Zaposleni {
+
+    
+    public Administrator() {
+    	super();
+    }
+    
+
+	@Override
+	public String toString() {
+		return "Administrator [korisnickoIme=" + korisnickoIme + ", korisnickaLozinka=" + korisnickaLozinka + ", plata="
+				+ plata + ", id=" + id + ", ime=" + ime + ", prezime=" + prezime + ", JMBG=" + JMBG + ", adresa="
+				+ adresa + ", pol=" + pol + "]";
+	}
+    
+    
+
+}
---
+++ Pol.java
@@ -0,0 +1,9 @@
+package osobe;
+
+public enum Pol {
+	
+	ZENSKI,   //0
+    MUSKI     //1
+    
+
+}
